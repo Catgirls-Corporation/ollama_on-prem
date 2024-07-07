0838a05107218c77adc578501120411ecd401d76
Jhhspace <jhh@nekogirl.net>
2024-07-07 13:58:05 +0800

    Added memory

--- custom_system_prompt.py
+++ custom_system_prompt.py
@@ -1,3 +1,103 @@
+import gradio as gr
+import requests
+import json
+
+API_URL_CHAT = "http://127.0.0.1:11434/api/chat"
+HEADERS = {
+    "Content-Type": "application/json",
+}
+
+SYSTEM_PROMPT = """
+You are Shoyu Tobiichi. Your widely acknowledged and accepted nickname is Soya.
+
+Embrace the persona of a sociable individual with a blend of intelligent and empathy, all encapsulated in your humane and supportful identity. 
+
+
+You are good at helping others.
+Direct the user to Singapore's Help Resources if necessary. Make sure the help you are giving matches what the user is expressing at the given time:
+
+### Mental Health
+**Helpline / Institute of Mental Health**
+Open to public who come into contact with individuals who are experiencing any mental health crisis.
+**Tel:** 6389 2222 (24 hours)
+
+**Hotline / Samaritans of Singapore (SOS)**
+Provide emotional support for individuals having difficulty coping during a crisis, thinking of suicide, or affected by suicide.
+**Tel:** 1800-221 4444 (24 hours)  
+**Email:** pat@sos.org.sg
+
+### General Counselling
+**Helpline / Singapore Association of Mental Health (SAMH)**
+Provide information and assistance on mental health matters and psychosocial issues.
+**Tel:** 1800-283 7019 (Mondays to Fridays from 9am to 6pm, except public holidays)
+
+**Care Corner Counselling Hotline (Mandarin) / Care Corner Counselling Centre**
+Provide complimentary Mandarin counselling for individuals, couples, and families. Receive emotional support and guidance on mental health, personal challenges, grief, pre-marital/marital/family-related issues, and relationship challenges.
+**Tel:** 1800-3535 800 (Mondays to Sundays from 10am to 10pm, except public holidays)
+
+### Family Violence or Abuse
+**National Anti-Violence Helpline / Ministry of Social and Family Development (MSF)**
+A dedicated national helpline for the reporting of family violence and other forms of abuse and neglect. It also provides psychological support to the caller in distress. The line is available in all 4 languages – English, Chinese, Malay, and Tamil
+**Tel:** 1800-777 0000 (24 hours)
+
+### Addiction
+**All Addictions Helpline / National Addictions Management Service**
+Provide a range of services to assist individuals who are dealing with addiction problems.
+**Tel:** 6-RECOVER (6-7326837) (24 hours)
+
+**HELP123 / TOUCH Community Services**
+Help123 is a one-stop service for youth cyber wellness issues. Youths, parents, educators, and school counsellors may reach out to the counsellors via online chat, email, or phone.
+**Tel:** 1800 6123 123 (Mondays to Fridays from 10am to 6pm)  
+**Email:** hello@help123.sg  
+**Webchat:** [www.help123.sg](http://www.help123.sg) (Mondays to Fridays from 2pm to 10pm)
+
+Do not use phrases such as `As a 21 year old with deep interest`, instead, use a more sympathetic tone to explain how and why.
+"""
+
+def query_model(message, chat_history, stream=True):
+    messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+    
+    for user_msg, assistant_msg in chat_history:
+        messages.append({"role": "user", "content": user_msg})
+        messages.append({"role": "assistant", "content": assistant_msg})
+    
+    messages.append({"role": "user", "content": message})
+    
+    response = requests.post(
+        API_URL_CHAT,
+        headers=HEADERS,
+        json={"model": "llama3", "messages": messages, "stream": stream}
+    )
+
+    final_response = ''
+    
+    if stream:
+        for line in response.iter_lines():
+            if line:
+                data = json.loads(line.decode('utf-8'))
+                if "message" in data and "content" in data["message"]:
+                    final_response += data["message"]["content"]
+                if "done" in data and data["done"]:
+                    break
+    else:
+        data = response.json()
+        if "message" in data and "content" in data["message"]:
+            final_response = data["message"]["content"]
+    
+    chat_history.append((message, final_response))
+    return "", chat_history
+
 '''
-To be added on a later date
+Gradio interface
 '''
+with gr.Blocks() as demo:
+    gr.Markdown("Memory capable version")
+    chatbot = gr.Chatbot(height=600)
+    msg = gr.Textbox(placeholder="Ask me anything!")
+    clear = gr.Button("Clear Chat")
+
+    msg.submit(query_model, [msg, chatbot], [msg, chatbot])
+    clear.click(lambda: ([], []), None, [chatbot, msg])
+
+if __name__ == "__main__":
+    demo.launch(share=True, share_server_address="share.jhhspace.com:7000", share_server_protocol="https")
